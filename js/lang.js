0d69d61cd7df560a39bd9701031b7c08923f36df
Dmitry Skokov <skokovdmitry97@gmail.com>
2023-04-10 13:44:41 +0500

    Добавил смену языка также и для мобилки, немного стилезовал

--- js/lang.js
+++ js/lang.js
@@ -1,91 +1,95 @@
 const langArr = {
-    "title" :  {
-        "ru": "Скоков Дмитрий, визитная карточка.",
-        "en": "Skokov Dmitry CV",
-    }, 
-    
-    "home" :  {
-        "ru": "Главная",
-        "en": "Home",
-    }, 
+  title: {
+    ru: 'Скоков Дмитрий, визитная карточка.',
+    en: 'Skokov Dmitry CV',
+  },
 
-    "about-me" :  {
-        "ru": "Обо мне",
-        "en": "About me",
-    }, 
+  home: {
+    ru: 'Главная',
+    en: 'Home',
+  },
 
-    "skills" :  {
-        "ru": "Умения",
-        "en": "Skills",
-    }, 
+  'about-me': {
+    ru: 'Обо мне',
+    en: 'About me',
+  },
 
-    "portfolio" :  {
-        "ru": "Портфолио",
-        "en": "Portfolio",
-    }, 
+  skills: {
+    ru: 'Умения',
+    en: 'Skills',
+  },
 
-    "cpontacts" :  {
-        "ru": "Контакты",
-        "en": "Contacts",
-    }, 
+  portfolio: {
+    ru: 'Портфолио',
+    en: 'Portfolio',
+  },
 
-    "front" :  {
-        "ru": "Фронтенд разработчик",
-        "en": "Frontend developer",
-    }, 
+  cpontacts: {
+    ru: 'Контакты',
+    en: 'Contacts',
+  },
 
-    "age" :  {
-        "ru": "25 лет, Екатеринбург",
-        "en": "25 years old, Yekaterinburg",
-    }, 
-    
-    "about-me-h1": {
-        "ru": "Обо мне",
-        "en": "About me",
-    },
+  front: {
+    ru: 'Фронтенд разработчик',
+    en: 'Frontend developer',
+  },
 
-    "about-me-1p" :  {
-        "ru": "Привет я Дима",
-        "en": "Hi, I'm Denis – UX/UI designer from Minsk. I'm interested in design and everything connected with it."
-    }, 
+  age: {
+    ru: '25 лет, Екатеринбург',
+    en: '25 years old, Yekaterinburg',
+  },
 
-    "about-me-2p" :  {
-        "ru": "Я учусь сам",
-        "en": "I'm studying at courses 'Web and mobile design interfaces' in IT-Academy.",
-    }, 
+  'about-me-h1': {
+    ru: 'Обо мне',
+    en: 'About me',
+  },
 
-    "about-me-3p" :  {
-        "ru": "Готов учиться",
-        "en": "Ready to implement excellent projects with wonderful people.",
-    }, 
+  'about-me-1p': {
+    ru: 'Привет я Дима',
+    en: "Hi, I'm Denis – UX/UI designer from Minsk. I'm interested in design and everything connected with it.",
+  },
 
-    "skills-h1" :  {
-        "ru": "Умения",
-        "en": "Skills",
-    }, 
+  'about-me-2p': {
+    ru: 'Я учусь сам',
+    en: "I'm studying at courses 'Web and mobile design interfaces' in IT-Academy.",
+  },
 
-    "skills-text" :  {
-        "ru": "Я работаю в программах ниже",
-        "en": "I work in such programs as",
-    }, 
+  'about-me-3p': {
+    ru: 'Готов учиться',
+    en: 'Ready to implement excellent projects with wonderful people.',
+  },
 
-    "contacts-h1" :  {
-        "ru": "Контакты",
-        "en": "Contacts",
-    }, 
+  'skills-h1': {
+    ru: 'Умения',
+    en: 'Skills',
+  },
 
-    "contacts-p1" :  {
-        "ru": "Хочешь узнать побольше и просто поболтать?",
-        "en": "Want to know more or just chat?",
-    }, 
+  'skills-text': {
+    ru: 'Я работаю в программах ниже',
+    en: 'I work in such programs as',
+  },
+  'contacts-button': {
+    ru: 'Отправить сообщение',
+    en: 'Send message',
+  },
 
-    "contacts-p2" :  {
-        "ru": "Пожалуйста!",
-        "en": "You are welcome!",
-    }, 
+  'contacts-h1': {
+    ru: 'Контакты',
+    en: 'Contacts',
+  },
 
-    "contacts__links-p1" :  {
-        "ru": "Следите за мной в",
-        "en": "Follow me on",
-    }, 
-}   
+  'contacts-p1': {
+    ru: 'Хочешь узнать побольше или просто поболтать?',
+    en: 'Want to know more or just chat?',
+  },
+
+  'contacts-p2': {
+    ru: 'Пожалуйста!',
+    en: 'You are welcome!',
+  },
+
+  'contacts__links-p1': {
+    ru: 'Следите за мной в',
+    en: 'Follow me on',
+  },
+}
